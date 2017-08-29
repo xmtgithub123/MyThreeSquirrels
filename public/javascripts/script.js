23056e6bf739c632082d4fe9a0921091cfd3368c
xmt <1018915057@qq.com>
2017-08-29 17:24:17 +0800

    add click event

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,6 +9,7 @@ $(function(){
   		$(".pao_title").hide();
   		$(".shou_ta").show();
 	}, false); 
+
 	var handler = document.getElementById("shou_ta");
 	handler.addEventListener("webkitAnimationEnd", function(){
 		$(".shou_ta").removeClass("fadeInUp");
@@ -16,9 +17,8 @@ $(function(){
   		$(".waibu_he").addClass("box_right"); //盒子右边滑动	
 	},false);
 
-
 	 var ToRightThen = document.getElementsByClassName("waibu_he")[0];
-	 console.log(ToRightThen)
+	 //console.log(ToRightThen)
 	 ToRightThen.addEventListener("webkitAnimationEnd", function(){
 		$(".shou_ta").hide();
   		$(".waibu_he").hide();
@@ -40,11 +40,49 @@ $(function(){
 	 	$(".tamll_pao").show();
 	 },false);
 
+
+     //点击开始拼按钮、显示遮罩
 	 $('#start_but').bind("click",function(){
 	 	$('.meng_box').show()
+        start_but_fuc();
 	 })
+
+     //点击蒙层，其消失
+     $('.meng_box').bind("click",function() {
+        $(this).hide();
+     })
 	
+    $('#mybut_right').bind("click",function(){
+        mybut_right_fuc()
+    })
 })
+
+
+var temple = 1;//盒子类型有3个
+var fuyu = 0;//祝福语8个
+var buble = 0;//气泡6个
+var word = "";//气泡上的文字
+var timeID;
+var substring;//选择盒子款示 点击右前头
+
+function mybut_right_fuc(){
+    var oldTemple = temple ;
+    temple--;
+    if(temple == 0) temple=3;
+    var innerhtml = "";
+    var innerhtml2 = "";
+
+
+}
+//显示步骤
+function start_but_fuc() {
+    $("#first_box").hide();
+    $("#animate_box").hide();
+    $("#second_box").show();
+    $("#start_pin").show(); 
+    $(".xuanze_name").show();
+    $(".word_bg").css("z-index","3");
+}
 
 function loading() {
 	$("#page1").animate({
